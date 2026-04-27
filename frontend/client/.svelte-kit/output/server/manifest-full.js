2ea1820b88ab4ef6cd884eeb695317897b93942f
fnbusiness2025 <yaccouv@fnbusinessconsultants.com>
2026-04-27 13:56:29 +0200

    fix: use node to execute vite directly to bypass Vercel permission issues

--- frontend/client/.svelte-kit/output/server/manifest-full.js
+++ frontend/client/.svelte-kit/output/server/manifest-full.js
@@ -10,7 +10,7 @@ return {
 	assets: new Set(["tcp.jpeg"]),
 	mimeTypes: {".jpeg":"image/jpeg"},
 	_: {
-		client: {start:"_app/immutable/entry/start.DVaUdnvw.js",app:"_app/immutable/entry/app.DcYZrVh4.js",imports:["_app/immutable/entry/start.DVaUdnvw.js","_app/immutable/chunks/2j07-f4E.js","_app/immutable/chunks/z4Zar4gR.js","_app/immutable/entry/app.DcYZrVh4.js","_app/immutable/chunks/z4Zar4gR.js","_app/immutable/chunks/Di6zslmw.js","_app/immutable/chunks/CsaauQz1.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
+		client: {start:"_app/immutable/entry/start.Dv_9BaRf.js",app:"_app/immutable/entry/app.Dou8sT3u.js",imports:["_app/immutable/entry/start.Dv_9BaRf.js","_app/immutable/chunks/DyYvpg-V.js","_app/immutable/chunks/z4Zar4gR.js","_app/immutable/entry/app.Dou8sT3u.js","_app/immutable/chunks/z4Zar4gR.js","_app/immutable/chunks/Di6zslmw.js","_app/immutable/chunks/CsaauQz1.js"],stylesheets:[],fonts:[],uses_env_dynamic_public:false},
 		nodes: [
 			__memo(() => import('./nodes/0.js')),
 			__memo(() => import('./nodes/1.js')),
--- frontend/client/.svelte-kit/output/server/nodes/0.js
+++ frontend/client/.svelte-kit/output/server/nodes/0.js
@@ -3,6 +3,6 @@
 export const index = 0;
 let component_cache;
 export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.BLWokLf9.js","_app/immutable/chunks/z4Zar4gR.js","_app/immutable/chunks/2j07-f4E.js","_app/immutable/chunks/CsaauQz1.js","_app/immutable/chunks/Cu4KaFVL.js"];
+export const imports = ["_app/immutable/nodes/0.BRZs6PXS.js","_app/immutable/chunks/z4Zar4gR.js","_app/immutable/chunks/DyYvpg-V.js","_app/immutable/chunks/CsaauQz1.js","_app/immutable/chunks/Cu4KaFVL.js"];
 export const stylesheets = ["_app/immutable/assets/0.CM9ucWVd.css"];
 export const fonts = [];
